84bcaff1c848dfdb4fd5bee0bc5f06c7f6e6c521
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2024-02-14 20:20:17 +0530

    Update JS.js

--- JS.js
+++ JS.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Designer", "Public Speaker", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Designerr", "Public Speake", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
